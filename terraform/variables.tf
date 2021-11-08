09a8ff840edbc3692507a760a8fd3ff9d7d0e46e
cb-seenivasan <cb-seenivasan@CBIT01644.local>
2021-11-08 14:14:19 +0530

    Test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,25 +4,25 @@
 
 variable "cluster-name" {
   default = "terraform-eks-demo"
-  type    = "string"
+  type    = string
 }
 
 variable "cluster-size" {
   default = "1"
-  type    = "string"
+  type    = string
 }
 
 variable "cluster-region" {
   default = "us-west-2"
-  type    = "string"
+  type    = string
 }
 
 variable "cluster-instance-type" {
   default = "m4.large"
-  type    = "string"
+  type    = string
 }
 
 variable "cluster-key-name" {
   default = ""
-  type    = "string"
+  type    = string
 }
